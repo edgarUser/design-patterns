711688e51316087d89ed1cf2f9e369c26ea97095
edgarUser <egdar.lopez.isc@gmail.com>
2020-12-21 15:56:47 -0600

    Was added the implementation of pattern

---
+++ behavioral-strategy/src/main/java/com/mx/ed/FakeQuack.java
@@ -0,0 +1,7 @@
+package com.mx.ed;
+
+public class FakeQuack implements QuackBehavior {
+	public void quack() {
+		System.out.println("Qwak");
+	}
+}
---
+++ behavioral-strategy/src/main/java/com/mx/ed/FlyBehavior.java
@@ -0,0 +1,5 @@
+package com.mx.ed;
+
+public interface FlyBehavior {
+	public void fly();
+}
